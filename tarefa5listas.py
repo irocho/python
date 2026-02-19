b1f84a72011f4ea3691706fc2a9d8c982136432d
irocho <lucaspeot@gmail.com>
2026-02-19 12:23:42 +0100

    Documentando tarefa5

--- tarefa5listas.py
+++ tarefa5listas.py
@@ -1,4 +1,11 @@
+"""
+Docstring para tarefa5listas
+
+Una lista de la fruta
+"""
+
+
 
 listita = ["apple", "banana", "cherry", "apple", "cherry"]
-print(len(listita))
-print(listita[1])
+print(len(listita)) # Cuenta el numero de elementos de la lista
+print(listita[1])   # Escribe el primer elmento de la lista
